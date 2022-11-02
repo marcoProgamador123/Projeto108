e3f30f40a93ce87c01efbb095c0df1b66cd53a5b
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-11-02 13:39:28 -0300

    concluido

--- sign_language.py
+++ sign_language.py
@@ -8,6 +8,7 @@ cap = cv2.VideoCapture(0)
 
 finger_tips =[8, 12, 16, 20]
 thumb_tip= 4
+finger_fold_status = []
 
 while True:
     ret,img = cap.read()
@@ -23,10 +24,23 @@ while True:
             for id ,lm in enumerate(hand_landmark.landmark):
                 lm_list.append(lm)
 
-             #O código vai aqui  
+             #O código vai aqui
+             
+        for tip in finger_tips:
+            x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
+            cv2.circle(img, (x,y),10, (255,0,0),cv2.FILLED)
+            
+            if lm_list[tip].x < lm_list[tip - 3].x:
+                cv2.circle(img, (x,y),10,(0,255,0),cv2.FILLED)
+                finger_fold_status.append(True)
+            else:
+                finger_fold_status.append(False)        
 
-
-
+            if all(finger_fold_status):
+                if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip - 2].y:
+                    print("curti")
+                    cv2.putText(img,"CURTI", (20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
+            
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
             mp_draw.DrawingSpec((0,255,0),4,2))
